9f6d38fc06fe5c09a7111b15b923e73e362604b3
CuteITGuy <grisetroseciel@gmail.com>
2016-01-10 18:41:05 +0700

    First commit

---
+++ HealthyProgrammerCOOL/HealthyProgrammerClock.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+
+namespace HealthyProgrammerCOOL
+{
+    public class HealthyProgrammerClock
+    {
+        #region Fields
+        private int _count;
+
+        private string _message;
+        private readonly SynchronizationContext _synchronizationContext;
+
+        private readonly Timer _timer = new Timer
+        {
+            AutoReset = false,
+        };
+        #endregion
+
+
+        #region  Constructors & Destructor
+        public HealthyProgrammerClock(SynchronizationContext synchronizationContext)
+        {
+            _synchronizationContext = synchronizationContext;
+            _timer.Elapsed += Timer_Elapsed;
+        }
+        #endregion
+
+
+        #region Events
+        public event MessageEventHandler Elapsed;
+        #endregion
+
+
+        #region Methods
+        public void Resume()
+        {
+            SetNextElapse();
+            _timer.Start();
+        }
+
+        public void Start()
+        {
+            ResetElapse();
+            _timer.Start();
+        }
+        #endregion
+
+
+        #region Event Handlers
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (_synchronizationContext != null)
+            {
+                _synchronizationContext.Send(state => OnElapsed(_message), null);
+            }
+            else
+            {
+                OnElapsed(_message);
+            }
+        }
+        #endregion
+
+
+        #region Implementation
+        protected virtual void OnElapsed(string message)
+        {
+            Elapsed?.Invoke(this, _message);
+        }
+
+        private void ResetElapse()
+        {
+            _count = 0;
+            SetNextElapse();
+        }
+
+        private void SetNextElapse()
+        {
+            _count = (_count + 1) % 4;
+
+            switch (_count)
+            {
+                case 0:
+                    _message = "Time to walk!";
+                    _timer.Interval = 25000;
+                    break;
+
+                case 1:
+                case 3:
+                    _message = "Time to change your posture!";
+                    _timer.Interval = 20000;
+                    break;
+
+                case 2:
+                    _message = "Time to stand up and relax your eyes!";
+                    _timer.Interval = 20000;
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+        #endregion
+    }
+}
---
+++ HealthyProgrammerCOOL/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+using Windows.UI.Notifications;
+using CB.Windows.UI;
+
+
+namespace HealthyProgrammerCOOL
+{
+    public partial class MainWindow
+    {
+        #region Fields
+        private const string TOAST_TITLE = "Healthy Programmer COOL";
+        private HealthyProgrammerClock _clock;
+        private Toast _toast;
+        #endregion
+
+
+        #region  Constructors & Destructor
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeMore();
+        }
+        #endregion
+
+
+        #region Event Handlers
+        private void Clock_Elapsed(object sender, string message)
+        {
+            _toast.Lines = new[] { TOAST_TITLE, message };
+            _toast.Show();
+        }
+
+        private void Toast_Activated(ToastNotification sender, object args)
+        {
+            _clock.Resume();
+        }
+
+        private void Toast_Dismissed(ToastNotification sender, ToastDismissedEventArgs args)
+        {
+            _clock.Resume();
+        }
+        #endregion
+
+
+        #region Implementation
+        private void InitializeMore()
+        {
+            _clock = new HealthyProgrammerClock(SynchronizationContext.Current);
+            _clock.Elapsed += Clock_Elapsed;
+            _clock.Start();
+            _toast = new Toast();
+            _toast.Activated += Toast_Activated;
+            _toast.Dismissed += Toast_Dismissed;
+        }
+        #endregion
+    }
+
+    public delegate void MessageEventHandler(object sender, string message);
+}
